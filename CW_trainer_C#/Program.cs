0a9705d0a660763da52dc512c1343eab1884db64
Rich5252 <richard.lax7@gmail.com>
2026-06-21 19:55:34 +0100

    Remove default Form and replace with the MainForm.Example

--- CW_trainer_C#/Program.cs
+++ CW_trainer_C#/Program.cs
@@ -1,3 +1,5 @@
+using CwTrainer;
+
 namespace CW_trainer_C_
 {
     internal static class Program
@@ -11,7 +13,7 @@ namespace CW_trainer_C_
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
